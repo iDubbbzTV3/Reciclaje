7dbab04dade5b6fecb9b3f5089d32e1d51d7cad8
00005016@uca.edu.sv <john200_@hotmail.com>
2018-04-19 18:26:49 -0600

    Initial commit

---
+++ app/src/main/java/Serie.java
@@ -0,0 +1,42 @@
+/**
+ * Created by UCA on 19/04/2018.
+ */
+
+public class Serie {
+    private String name;
+    private String caps;
+    private int img;
+    private String desc;
+
+    public Serie (String name, String caps, int img, String desc){
+        this.name = name;
+        this.caps = caps;
+        this.img = img;
+        this.desc = desc;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getCaps(){
+        return caps;
+    }
+    public void setCaps(String name){
+        this.caps = caps;
+    }
+    public String getDesc(){
+        return desc;
+    }
+    public void setDesc(String desc){
+        this.desc = desc;
+    }
+    public int getImg(){
+        return img;
+    }
+    public void setImg(int img){
+        this.img = img;
+    }
+}
---
+++ app/src/main/java/com/ecalles/reciclaje/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ecalles.reciclaje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    RecyclerView rv;
+    SeriesAdapter adapter;
+    ArrayList<Serie> series;
+    LinearLayoutManager iManager;
+}
